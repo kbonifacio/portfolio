c7507293db09dd36af1fa9798c8a47cb85f121ad
Kriselda Bonifacio <88959891+kbonifacio@users.noreply.github.com>
2022-04-01 00:35:25 -0700

    feat(home): created Home component and tested for functionality.

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,19 @@
 import React from "react";
 import { Routes, Route, NavLink } from "react-router-dom"
+import {
+    Home
+} from "./index"
 
 const App = () => {
     return (<>
-        <NavLink to="/">Home</NavLink>
-        <NavLink to="/projects">Projects</NavLink>
-        <NavLink to="/aboutme">About Me</NavLink>
-        <NavLink to="/contact">Contact</NavLink>
+        <NavLink to="/" className="headerLinks">Home</NavLink>
+        <NavLink to="/projects" className="headerLinks">Projects</NavLink>
+        <NavLink to="/aboutme" className="headerLinks">About Me</NavLink>
+        <NavLink to="/contact" className="headerLinks">Contact</NavLink>
+
+        <Routes>
+            <Route path="/" exact element={ <Home/>}/>
+        </Routes>
     </>)
 }
 
---
+++ src/components/Home.js
@@ -0,0 +1,9 @@
+import React from "react";
+import "./Home.css"
+
+const Home = () => {
+    return (
+        <div className="header">"Hello World"</div>
+    )
+}
+export default Home;
