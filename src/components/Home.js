389cbb76e0817c934167c49aab74a2b6cc34cb25
Kriselda Bonifacio <88959891+kbonifacio@users.noreply.github.com>
2022-04-01 01:46:22 -0700

    feat(home): created base content for home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,23 @@
 import React from "react";
-import "./Home.css"
+import headProfile from "../images/headProfile.png";
+import "./Home.css";
 
 const Home = () => {
-    return (
-        <div className="header">"Hello World"</div>
-    )
+    return <>
+        <div className="outterComponentContainer">
+            <div className="innerComponentContainer">
+                <div className="introContainer">
+                    <div className="imgContainer">
+                        <img src={headProfile} className="profilePic" alt="Kriselda's photo" />
+                    </div>
+                    <div className="textContainer">
+                        <p className="header">Hi there friend.</p>
+                        <p className="introText">My name is Kriselda Bonifacio. I am a junior software engineer with a bachelors of science in Biochemistry. I love diving into the unknown and learning new skills that could one day help the people around me.  </p>
+                        <p className="introText">Here you will find a record of my journey so far, my thought processes, ideas, and my aspirations.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </>
 }
 export default Home;
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
 import { App } from "./components"
+import "./style.css"
 
 ReactDOM.render(
     <Router>
