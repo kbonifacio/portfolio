1a1259d33a281f9a3dbb94667bcd6dd10f95a9b7
Kriselda Bonifacio <88959891+kbonifacio@users.noreply.github.com>
2022-04-03 15:04:04 -0700

    feat(Projectt(Projects): created component, added background, and created first project card(Porto3000)

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,10 +1,25 @@
 import React from "react";
-
+import "./Projects.css";
+import P01 from "../images/Porto3000/P01.png"
 const Projects = () => {
     return <>
-    
+        <div className="projectsbg">
+            <div className="projectsContainer">
+                <div className="projectCard">
+                    <h1 className="title">Grace Hopper: Porto3000 </h1>
+                    <div className="innerProjectCard">
+                        <div className="thumbnailContainer">
+                            <img src={P01} className="thumbnail"/>
+                        </div>
+                        <div className="textContainer">
+                            <p className="textDescription">A wine and cheese e-commerce website</p>
+                            <p className="projectStack"><b>Stack: </b> Javasript, HTML5, CSS3, React, Node.JS, Express, PostgreSQL </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </>
-
 }
 
 export default Projects;
