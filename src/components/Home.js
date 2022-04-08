04331ae9df3065c21ef02a72a7861b5ae7a50383
Kriselda Bonifacio <88959891+kbonifacio@users.noreply.github.com>
2022-04-07 20:25:13 -0700

    feat(Home): css redo for clean interface

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,6 +2,7 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 import headProfile from "../images/headProfile.png";
 import "./Home.css";
+import "animate.css";
 
 const Home = () => {
     return <>
@@ -12,8 +13,8 @@ const Home = () => {
                         <img src={headProfile} className="profilePic" alt="Kriselda's photo" />
                     </div>
                     <div className="textContainer">
-                        <p className="header">Hi there friend.</p>
-                        <p className="introText">My name is Kriselda Bonifacio. I am a junior software engineer with a bachelors of science in Biochemistry. I love diving into the unknown and learning new skills that could one day help the people around me.  </p>
+                        <p className="header">Hi, I'm Kriselda.</p>
+                        <p className="introText">I am a software engineer with a bachelors of science in Biochemistry. I love diving into the unknown and learning new skills that could one day help the people around me.  </p>
                         <p className="introText">Here you will find a record of my journey so far, my thought processes, ideas, and my aspirations.</p>
                     </div>
                 </div>
