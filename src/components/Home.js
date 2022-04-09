032d44055e3b7d4f545dc03a382539dac92a667c
Kriselda Bonifacio <88959891+kbonifacio@users.noreply.github.com>
2022-04-08 20:11:48 -0700

    feat(home): restyled navlinks with gradient

--- src/components/Home.js
+++ src/components/Home.js
@@ -13,9 +13,9 @@ const Home = () => {
                         <img src={headProfile} className="profilePic" alt="Kriselda's photo" />
                     </div>
                     <div className="textContainer">
-                        <p className="header">Hi, I'm Kriselda.</p>
+                        <p className="header">Hi, I'm <NavLink to="/aboutme" className="kriselda">Kriselda.</NavLink></p>
                         <p className="introText">I am a software engineer with a bachelors of science in Biochemistry. I love diving into the unknown and learning new skills that could one day help the people around me.  </p>
-                        <p className="introText">Here you will find a record of my journey so far, my thought processes, ideas, and my aspirations.</p>
+                        <p className="introText">Here you will find a record of my journey so far.</p>
                     </div>
                 </div>
             </div>
