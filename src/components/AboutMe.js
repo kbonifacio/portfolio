7d23e5c13d391460d58204783be32c884b9042a8
Kriselda Bonifacio <88959891+kbonifacio@users.noreply.github.com>
2022-04-11 00:19:54 -0700

    feat(aboutMe): added classNam text to all paragraph tags

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -1,12 +1,10 @@
 import React from "react";
-import headProfile from "../images/headProfile.png"
 import "./AboutMe.css"
 import "animate.css"
 
 const AboutMe = () => {
     return <>
         <div className="aboutContainer"> 
-            <div className="timelineL"></div>
             <div className="timelineR">
                 <div className="subhead">
                     <h1>My Growth Through the Years</h1>
@@ -14,61 +12,61 @@ const AboutMe = () => {
                 <div className="year">2011</div>
                 <div className="event">
                     <div>
-                        <p><b>August:</b> Accepted the position of Game Advisor with Gamestop Corportation</p>
-                        <p><b>What I learned:</b> The world is <i>huge.</i></p>
-                        <p>As an 18 year old, I began my journey into the gaming industry world (and the adult world). I learned first hand the process of "white glove" service in regard to customer experience. During this time, I learned how to observe and ask questions as I dove into the unknown with each new guest. I loved helping our guests feel secure and happy with not only their purchase, but their overall experience.  </p>
+                        <p className="text"><b>August:</b> Accepted the position of Game Advisor with Gamestop Corportation</p>
+                        <p className="text"><b>What I learned:</b> The world is <i>huge.</i></p>
+                        <p className="text">As an 18 year old, I began my journey into the gaming industry world (and the adult world). I learned first hand the process of "white glove" service in regard to customer experience. During this time, I learned how to observe and ask questions as I dove into the unknown with each new guest. I loved helping our guests feel secure and happy with not only their purchase, but their overall experience.  </p>
 
                     </div>
                 </div>  
                 <div className="year">2013</div>
                 <div className="event">
                     <div>
-                        <p>July: Accepted the position as a Senior Game Advisor with Gamestop Corporation.</p>
-                        <p><b>What I learned:</b> How to be a <i>mentor.</i></p>
-                        <p>As a Senior Game Advisor, I was introduced to the concept of mentoring others. My position was the liason between entry level staff members and mangerial staff. In addition to my duties of tending to guests, I was able to train and guide the staff under my purvue. While managerial staff were off dudty, I maintained the title of "Manager on Duty", taking on additional responsibilities to care for my staff and guests. </p>
+                        <p className="text">July: Accepted the position as a Senior Game Advisor with Gamestop Corporation.</p>
+                        <p className="text"><b>What I learned:</b> How to be a <i>mentor.</i></p>
+                        <p className="text">As a Senior Game Advisor, I was introduced to the concept of mentoring others. My position was the liason between entry level staff members and mangerial staff. In addition to my duties of tending to guests, I was able to train and guide the staff under my purvue. While managerial staff were off dudty, I maintained the title of "Manager on Duty", taking on additional responsibilities to care for my staff and guests. </p>
                     </div>
                 </div>  
                 <div className="year">2015</div>
                 <div className="event">
                     <div>
-                        <p>October: Accepted the position as an Assistant Store Manager with Gamestop Corporation.</p>
-                        <p><b>What I learned:</b> How to <i>lead.</i></p>
-                        <p>The position of Assistant Store Manager was a great experience. I learned how to balance my duties as a manager and delegate as appropriate to my staff. I spent a lot of time in this role self-reflecting on what the word "leader" meant. I came to the conclusion that a leader not only leads by example, but is also cognizant of the needs, wants, and aspirations of their staff. As as leader, I worked to guide my staff on the inner workings of our company as I learned how small changes can make a large impact on our success as a team. In addition to this, I encouraged my staff to take steps towards their goals within and outside of the company. I discovered that communication and genuine interest in the well-being of others is truly the foundation of a great working relationships.</p>
+                        <p className="text">October: Accepted the position as an Assistant Store Manager with Gamestop Corporation.</p>
+                        <p className="text"><b>What I learned:</b> How to <i>lead.</i></p>
+                        <p className="text">The position of Assistant Store Manager was a great experience. I learned how to balance my duties as a manager and delegate as appropriate to my staff. I spent a lot of time in this role self-reflecting on what the word "leader" meant. I came to the conclusion that a leader not only leads by example, but is also cognizant of the needs, wants, and aspirations of their staff. As as leader, I worked to guide my staff on the inner workings of our company as I learned how small changes can make a large impact on our success as a team. In addition to this, I encouraged my staff to take steps towards their goals within and outside of the company. I discovered that communication and genuine interest in the well-being of others is truly the foundation of a great working relationships.</p>
                     </div>
                 </div>  
                 <div className="year">2017</div>
                 <div className="event">
                     <div>
-                        <p>October: Began State Civil Service as an Office Technician with California Correctional Health Care Services.</p>
-                        <p><b>What I learned:</b> The world is even <i>bigger</i> than I thought.</p>
-                        <p>I found myself in a new place with curious things surrounding me once again. I dove into the world of correctional medical services and learned how to work within an office setting. I currently report to the Chief Support Executive, supporting approximately 300+ staff members accross varioud departments. I continue to learn about how to deliver quality health care. I prepare and submit internal audits as requested by managerial staff or outside auditors, complete data analysis for multiple committes, and manage access requests for staff accross various departments. This was a humbling experience that I will hold onto forever. I learned that I will never fully understand what the person next to me may be facing and that life is precious.  </p>
+                        <p className="text">October: Began State Civil Service as an Office Technician with California Correctional Health Care Services.</p>
+                        <p className="text"><b>What I learned:</b> The world is even <i>bigger</i> than I thought.</p>
+                        <p className="text">I found myself in a new place with curious things surrounding me once again. I dove into the world of correctional medical services and learned how to work within an office setting. I currently report to the Chief Support Executive, supporting approximately 300+ staff members accross varioud departments. I continue to learn about how to deliver quality health care. I prepare and submit internal audits as requested by managerial staff or outside auditors, complete data analysis for multiple committes, and manage access requests for staff accross various departments. This was a humbling experience that I will hold onto forever. I learned that I will never fully understand what the person next to me may be facing and that life is precious.  </p>
                     </div>
                 </div>  
                 <div className="year">2018</div>
                 <div className="event">
                     <div>
-                        <p>Began attending California State University, Sacramento, to study Biochemistry.</p>
-                        <p><b>What I learned:</b> The Academic world is <i>great</i> exposure.</p>
-                        <p>The stress of pursuing higher education is intense. I met so many new people and a wide array of new experiences. I learned how to hone my craft as an aspiring lab professional by spending hours within the lab conducting research. I researched wild life during field studies, the water quality of the Sacramento river, alzheimer's disease, and SARS-CoV-2 (COVID-19). The technical experience and soft skills I gained while at Sac State have guided me in my work and in other avenues of life.   </p>
+                        <p className="text">Began attending California State University, Sacramento, to study Biochemistry.</p>
+                        <p className="text"><b>What I learned:</b> The Academic world is <i>great</i> exposure.</p>
+                        <p className="text">The stress of pursuing higher education is intense. I met so many new people and a wide array of new experiences. I learned how to hone my craft as an aspiring lab professional by spending hours within the lab conducting research. I researched wild life during field studies, the water quality of the Sacramento river, alzheimer's disease, and SARS-CoV-2 (COVID-19). The technical experience and soft skills I gained while at Sac State have guided me in my work and in other avenues of life.   </p>
                     </div>
                 </div>  
                 <div className="year">2021</div>
                 <div className="event">
                     <div>
-                        <p>August: Began 26-web developement program through FullStack Academy</p>
-                        <p><b>What I learned:</b> I have a <i>passion</i> for programming.</p>
-                        <p>Prior to my last semester at Sacramento State, I was introduced to coding through Swift Playgrounds for MacOS. I immediately was engrossed with solving the main character's problems through coding in Swift. It was not long after that I found that FullStack Academy was hosting a new 26-week web development program through California State East Bay. I applied and decided to leap into the world of coding. I realized that I had found a career field that I truly saw myself being a part of and loving it. </p>
-                        <p>December: Completed Biochemistry, B.S., degree through California State University, Sacramento</p>
-                        <p><b>What I learned:</b> It is <i>thrilling</i> to complete a long term goal.</p>
-                        <p>Completing my degree later in my adult years was a different experience that I am thankful for. I was able to fulfill a long-term life goal and I was filled with pride. I am a first generation college student who hopes to inspire the younger children within my family to pursue their passions. My struggles and achievements will act as a guide and foundations for the journies of those around me.   </p>
+                        <p className="text">August: Began 26-web developement program through FullStack Academy</p>
+                        <p className="text"><b>What I learned:</b> I have a <i>passion</i> for programming.</p>
+                        <p className="text">Prior to my last semester at Sacramento State, I was introduced to coding through Swift Playgrounds for MacOS. I immediately was engrossed with solving the main character's problems through coding in Swift. It was not long after that I found that FullStack Academy was hosting a new 26-week web development program through California State East Bay. I applied and decided to leap into the world of coding. I realized that I had found a career field that I truly saw myself being a part of and loving it. </p>
+                        <p className="text">December: Completed Biochemistry, B.S., degree through California State University, Sacramento</p>
+                        <p className="text"><b>What I learned:</b> It is <i>thrilling</i> to complete a long term goal.</p>
+                        <p className="text">Completing my degree later in my adult years was a different experience that I am thankful for. I was able to fulfill a long-term life goal and I was filled with pride. I am a first generation college student who hopes to inspire the younger children within my family to pursue their passions. My struggles and achievements will act as a guide and foundations for the journies of those around me.   </p>
                     </div>
                 </div>
                 <div className="year">2022</div>
                 <div className="event"> 
                     <div>
-                        <p>February: Completed 26-web developement program through FullStack Academy</p>
-                        <p><b>What I learned:</b> I am <i>capable.</i> </p>
-                        <p>I discovered that I am capable. I struggled, persevered, and gained the knowledge required to develop an application from the ground up. Despite obtaining a degree in a different STEM major, I was able to apply the soft skills and study skills that I had learned throughout my college experience to be successful within the program. I experienced what it was like to work alongside other developers and develop and test code. I learned JavaScript, HTML5, CSS3, Angular, React, Node.JS, PostgreSQL, Single Page Application methodology, Agile methodology, Git, Netlify, Heroku, and more. </p>
+                        <p className="text">February: Completed 26-web developement program through FullStack Academy</p>
+                        <p className="text"><b>What I learned:</b> I am <i>capable.</i> </p>
+                        <p className="text">I discovered that I am capable. I struggled, persevered, and gained the knowledge required to develop an application from the ground up. Despite obtaining a degree in a different STEM major, I was able to apply the soft skills and study skills that I had learned throughout my college experience to be successful within the program. I experienced what it was like to work alongside other developers and develop and test code. I learned JavaScript, HTML5, CSS3, Angular, React, Node.JS, PostgreSQL, Single Page Application methodology, Agile methodology, Git, Netlify, Heroku, and more. </p>
                     </div>
                 </div>
             </div>
